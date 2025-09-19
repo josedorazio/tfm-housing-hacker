8dde088021a8ad814af84b63fcfca8d30e8859e4
José D'Orazio <149203231+josedorazio@users.noreply.github.com>
2025-09-19 09:24:37 +0200

    Develop (#14)
    * [chore: add pre-commit configuration for formatting
    
    * [chore]: import packages
    
    * [FEAT-APP]: Add app file to connect pages files
    
    * [FEAT-ENV] Add path from csv files to ENV to create Global variable (#2)
    
    * [FEAT-ENV]: Add requirements.txt
    
    * [FEAT-ENV]: Add .env file paths
    
    * [FEAT-ENV]: Add Clustering path
    
    ---------
    
    Co-authored-by: JoseDDA <josedorazio@gmai.com>
    
    * [FEAT-EDA] - Add EDA page to visualize data
    
    * [FEAT-UI]: Set up page and Impot dataset. Add shape and column Overview
    
    * [FEAT-UI]:Add head & tail preview
    
    * [FEAT-UI]: Add duplicate row count
    
    * [FEAT-VIZ]: Add numeric variable selection and plots. Add categorical variable dropdown and bar chart
    
    * [FEAT-VIZ]:Add correlation heatmap with numeric variable selection
    
    * [FEAT-ENV]: Add production .env loading
    
    ---------
    
    Co-authored-by: JoseDDA <josedorazio@gmai.com>
    
    * Feature/clustering:  Add clustering visualization
    
    * [FEAT-VIZ]: Add distribution visualization with dropbox option
    
    * [FEAT-DATA]: Add path to data files in .venv
    
    * [FEAT-chore]: Rename file to facilitate following
    
    * [FEAT-VIZ]: Geography Map visualization
    
    ---------
    
    Co-authored-by: JoseDDA <josedorazio@gmai.com>
    
    * [FEAT-DATA]: Add data files and path to .env
    
    * [FEAT-MODEL] - Create script for data, Create page (#10)
    
    Co-authored-by: JoseDDA <josedorazio@gmai.com>
    
    * Feature/mvp (#11)
    
    * [FEAT-MVP]: Create MVP with range price and size
    
    * [FEAT-COMPARABLE] - Add comparable feature
    
    ---------
    
    Co-authored-by: JoseDDA <josedorazio@gmai.com>
    
    * [FEAT-PRESENTATION] - Update main presentation
    
    ---------
    
    Co-authored-by: JoseDDA <josedorazio@gmai.com>

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-st.set_page_config(page_title="TFM Housing Hacker", page_icon="🏡", layout="wide")
-
-st.title("🏡 Housing Price Analysis")
-
-
-st.sidebar.title("Navigation")
